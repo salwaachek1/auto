0acbb9490b20073762096d007b4d251840cbadaf
salwa achek <salwaachek1@gmail.com>
2022-02-25 13:03:57 +0100

    fixing interfaces

--- resources/views/layouts/common.blade.php
+++ resources/views/layouts/common.blade.php
@@ -236,7 +236,7 @@ max-width: 100px;
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="{{ asset('storage/users/'Auth::user()->photo_url) }}" class="img-circle elevation-2" alt="User Image" >
+          <img src="{{ asset('storage/users/'.Auth::user()->photo_url) }}" class="img-circle elevation-2" alt="User Image" >
         </div>
         <div class="info">
           <a href="#" class="d-block">{{Auth::user()->name}}</a>
